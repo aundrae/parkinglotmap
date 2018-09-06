556766930be94bd37ac552a87a09cc1022e2f2c5
aundrae <36115062+aundrae@users.noreply.github.com>
2018-09-06 18:29:44 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/justi/bootcamp_uwi_parking/GoogleMapActivity.java
@@ -0,0 +1,12 @@
+package com.example.justi.bootcamp_uwi_parking;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GoogleMapActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity);
+    }
+}
